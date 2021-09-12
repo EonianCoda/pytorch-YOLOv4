ea78b8cd5c61d49b2038ea5ec45139ba13c118ad
EonianCoda <textuseful159000@gmail.com>
2021-09-12 16:00:01 +0800

    add evaluation

---
+++ validation.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+'''
+@Time          : 2020/05/06 15:07
+@Author        : Tianxiaomo
+@File          : train.py
+@Noice         :
+@Modificattion :
+    @Author    :
+    @Time      :
+    @Detail    :
+
+'''
+import time
+import logging
+import os, sys, math
+import argparse
+from collections import deque
+import datetime
+
+import cv2
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch import optim
+from torch.nn import functional as F
+from tensorboardX import SummaryWriter
+from easydict import EasyDict as edict
+
+from dataset import Yolo_dataset
+from cfg import Cfg
+from models import Yolov4
+from tool.darknet2pytorch import Darknet
+
+from tool.tv_reference.utils import collate_fn as val_collate
+from tool.tv_reference.coco_utils import convert_to_coco_api
+from tool.tv_reference.coco_eval import CocoEvaluator
+
+
+
+
+def collate(batch):
+    images = []
+    bboxes = []
+    for img, box in batch:
+        images.append([img])
+        bboxes.append([box])
+    images = np.concatenate(images, axis=0)
+    images = images.transpose(0, 3, 1, 2)
+    images = torch.from_numpy(images).div(255.0)
+    bboxes = np.concatenate(bboxes, axis=0)
+    bboxes = torch.from_numpy(bboxes)
+    return images, bboxes
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, cfg, device, logger=None, **kwargs):
+    """ finished, tested
+    """
+    # cpu_device = torch.device("cpu")
+    model.eval()
+    # header = 'Test:'
+
+    coco = convert_to_coco_api(data_loader.dataset, bbox_fmt='coco')
+    coco_evaluator = CocoEvaluator(coco, iou_types = ["bbox"], bbox_fmt='coco')
+
+    for images, targets in data_loader:
+        model_input = [[cv2.resize(img, (cfg.w, cfg.h))] for img in images]
+        model_input = np.concatenate(model_input, axis=0)
+        model_input = model_input.transpose(0, 3, 1, 2)
+        model_input = torch.from_numpy(model_input).div(255.0)
+        model_input = model_input.to(device)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        model_time = time.time()
+        outputs = model(model_input)
+
+        # outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        model_time = time.time() - model_time
+
+        # outputs = outputs.cpu().detach().numpy()
+        res = {}
+        # for img, target, output in zip(images, targets, outputs):
+        for img, target, boxes, confs in zip(images, targets, outputs[0], outputs[1]):
+            img_height, img_width = img.shape[:2]
+            # boxes = output[...,:4].copy()  # output boxes in yolo format
+            boxes = boxes.squeeze(2).cpu().detach().numpy()
+            boxes[...,2:] = boxes[...,2:] - boxes[...,:2] # Transform [x1, y1, x2, y2] to [x1, y1, w, h]
+            boxes[...,0] = boxes[...,0]*img_width
+            boxes[...,1] = boxes[...,1]*img_height
+            boxes[...,2] = boxes[...,2]*img_width
+            boxes[...,3] = boxes[...,3]*img_height
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            # confs = output[...,4:].copy()
+            confs = confs.cpu().detach().numpy()
+            labels = np.argmax(confs, axis=1).flatten()
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+            scores = np.max(confs, axis=1).flatten()
+            scores = torch.as_tensor(scores, dtype=torch.float32)
+            res[target["image_id"].item()] = {
+                "boxes": boxes,
+                "scores": scores,
+                "labels": labels,
+            }
+        evaluator_time = time.time()
+        coco_evaluator.update(res)
+        evaluator_time = time.time() - evaluator_time
+
+    # gather the stats from all processes
+    coco_evaluator.synchronize_between_processes()
+
+    # accumulate predictions from all images
+    coco_evaluator.accumulate()
+    coco_evaluator.summarize()
+
+    return coco_evaluator
+
+
+def evaluation(model, device, config):
+    val_dataset = Yolo_dataset(config.val_label, config, train=False)
+
+    val_loader = DataLoader(val_dataset, batch_size=config.batch // config.subdivisions, shuffle=True, num_workers=8,
+                            pin_memory=True, drop_last=True, collate_fn=val_collate)
+    # Evaluation
+    if cfg.use_darknet_cfg:
+        eval_model = Darknet(cfg.cfgfile, inference=True)
+    else:
+        eval_model = Yolov4(cfg.pretrained, n_classes=cfg.classes, inference=True)
+    if torch.cuda.device_count() > 1:
+        eval_model.load_state_dict(model.module.state_dict())
+    else:
+        eval_model.load_state_dict(model.state_dict())
+    eval_model.to(device)
+    evaluator = evaluate(eval_model, val_loader, config, device)
+    del eval_model
+
+
+def get_args(**kwargs):
+    cfg = kwargs
+    parser = argparse.ArgumentParser(description='Train the Model on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
+    #                     help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
+                        help='Learning rate', dest='learning_rate')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=None,
+                        help='Load model from a .pth file')
+    parser.add_argument('-g', '--gpu', metavar='G', type=str, default='-1',
+                        help='GPU', dest='gpu')
+    parser.add_argument('-dir', '--data-dir', type=str, default=None,
+                        help='dataset dir', dest='dataset_dir')
+    parser.add_argument('-pretrained', type=str, default=None, help='pretrained yolov4.conv.137')
+    parser.add_argument('-classes', type=int, default=80, help='dataset classes')
+    parser.add_argument('-train_label_path', dest='train_label', type=str, default='train.txt', help="train label path")
+    parser.add_argument(
+        '-optimizer', type=str, default='adam',
+        help='training optimizer',
+        dest='TRAIN_OPTIMIZER')
+    parser.add_argument(
+        '-iou-type', type=str, default='iou',
+        help='iou type (iou, giou, diou, ciou)',
+        dest='iou_type')
+    parser.add_argument(
+        '-keep-checkpoint-max', type=int, default=10,
+        help='maximum number of checkpoints to keep. If set 0, all checkpoints will be kept',
+        dest='keep_checkpoint_max')
+    args = vars(parser.parse_args())
+
+    # for k in args.keys():
+    #     cfg[k] = args.get(k)
+    cfg.update(args)
+
+    return edict(cfg)
+
+
+if __name__ == "__main__":
+    cfg = get_args(**Cfg)
+    os.environ["CUDA_VISIBLE_DEVICES"] = cfg.gpu
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+
+    if cfg.use_darknet_cfg:
+        model = Darknet(cfg.cfgfile)
+    else:
+        model = Yolov4(cfg.pretrained, n_classes=cfg.classes)
+
+    try:
+        evaluation(model=model,
+                    config=cfg,
+                    device=device)
+    except Exception as e:
+        print(e)
+        exit(-1)
+        
