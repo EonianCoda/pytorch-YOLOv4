e26bde20c85f17fc2df75b0445c230ccb8a141a9
EonianCoda <textuseful159000@gmail.com>
2021-09-12 16:28:14 +0800

    change validation

--- validation.py
+++ validation.py
@@ -65,50 +65,52 @@ def evaluate(model, data_loader, cfg, device, logger=None, **kwargs):
     coco = convert_to_coco_api(data_loader.dataset, bbox_fmt='coco')
     coco_evaluator = CocoEvaluator(coco, iou_types = ["bbox"], bbox_fmt='coco')
 
-    for images, targets in data_loader:
-        model_input = [[cv2.resize(img, (cfg.w, cfg.h))] for img in images]
-        model_input = np.concatenate(model_input, axis=0)
-        model_input = model_input.transpose(0, 3, 1, 2)
-        model_input = torch.from_numpy(model_input).div(255.0)
-        model_input = model_input.to(device)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        if torch.cuda.is_available():
-            torch.cuda.synchronize()
-        model_time = time.time()
-        outputs = model(model_input)
+    with tqdm(total=len(data_loader)) as pbar:
+        for idx, (images, targets) in enumerate(data_loader):
+            model_input = [[cv2.resize(img, (cfg.w, cfg.h))] for img in images]
+            model_input = np.concatenate(model_input, axis=0)
+            model_input = model_input.transpose(0, 3, 1, 2)
+            model_input = torch.from_numpy(model_input).div(255.0)
+            model_input = model_input.to(device)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        # outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
-        model_time = time.time() - model_time
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+            model_time = time.time()
+            outputs = model(model_input)
 
-        # outputs = outputs.cpu().detach().numpy()
-        res = {}
-        # for img, target, output in zip(images, targets, outputs):
-        for img, target, boxes, confs in zip(images, targets, outputs[0], outputs[1]):
-            img_height, img_width = img.shape[:2]
-            # boxes = output[...,:4].copy()  # output boxes in yolo format
-            boxes = boxes.squeeze(2).cpu().detach().numpy()
-            boxes[...,2:] = boxes[...,2:] - boxes[...,:2] # Transform [x1, y1, x2, y2] to [x1, y1, w, h]
-            boxes[...,0] = boxes[...,0]*img_width
-            boxes[...,1] = boxes[...,1]*img_height
-            boxes[...,2] = boxes[...,2]*img_width
-            boxes[...,3] = boxes[...,3]*img_height
-            boxes = torch.as_tensor(boxes, dtype=torch.float32)
-            # confs = output[...,4:].copy()
-            confs = confs.cpu().detach().numpy()
-            labels = np.argmax(confs, axis=1).flatten()
-            labels = torch.as_tensor(labels, dtype=torch.int64)
-            scores = np.max(confs, axis=1).flatten()
-            scores = torch.as_tensor(scores, dtype=torch.float32)
-            res[target["image_id"].item()] = {
-                "boxes": boxes,
-                "scores": scores,
-                "labels": labels,
-            }
-        evaluator_time = time.time()
-        coco_evaluator.update(res)
-        evaluator_time = time.time() - evaluator_time
+            # outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+            model_time = time.time() - model_time
 
+            # outputs = outputs.cpu().detach().numpy()
+            res = {}
+            # for img, target, output in zip(images, targets, outputs):
+            for img, target, boxes, confs in zip(images, targets, outputs[0], outputs[1]):
+                img_height, img_width = img.shape[:2]
+                # boxes = output[...,:4].copy()  # output boxes in yolo format
+                boxes = boxes.squeeze(2).cpu().detach().numpy()
+                boxes[...,2:] = boxes[...,2:] - boxes[...,:2] # Transform [x1, y1, x2, y2] to [x1, y1, w, h]
+                boxes[...,0] = boxes[...,0]*img_width
+                boxes[...,1] = boxes[...,1]*img_height
+                boxes[...,2] = boxes[...,2]*img_width
+                boxes[...,3] = boxes[...,3]*img_height
+                boxes = torch.as_tensor(boxes, dtype=torch.float32)
+                # confs = output[...,4:].copy()
+                confs = confs.cpu().detach().numpy()
+                labels = np.argmax(confs, axis=1).flatten()
+                labels = torch.as_tensor(labels, dtype=torch.int64)
+                scores = np.max(confs, axis=1).flatten()
+                scores = torch.as_tensor(scores, dtype=torch.float32)
+                res[target["image_id"].item()] = {
+                    "boxes": boxes,
+                    "scores": scores,
+                    "labels": labels,
+                }
+            evaluator_time = time.time()
+            coco_evaluator.update(res)
+            evaluator_time = time.time() - evaluator_time
+            pbar.update()
     # gather the stats from all processes
     coco_evaluator.synchronize_between_processes()
 
@@ -122,7 +124,7 @@ def evaluate(model, data_loader, cfg, device, logger=None, **kwargs):
 def evaluation(model, device, config):
     val_dataset = Yolo_dataset(config.val_label, config, train=False)
 
-    val_loader = DataLoader(val_dataset, batch_size=config.batch // config.subdivisions, shuffle=True, num_workers=8,
+    val_loader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=True, num_workers=8,
                             pin_memory=True, drop_last=True, collate_fn=val_collate)
     # Evaluation
     if cfg.use_darknet_cfg:
@@ -142,10 +144,6 @@ def get_args(**kwargs):
     cfg = kwargs
     parser = argparse.ArgumentParser(description='Train the Model on images and target masks',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
-    #                     help='Batch size', dest='batchsize')
-    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
-                        help='Learning rate', dest='learning_rate')
     parser.add_argument('-f', '--load', dest='load', type=str, default=None,
                         help='Load model from a .pth file')
     parser.add_argument('-g', '--gpu', metavar='G', type=str, default='-1',
@@ -156,17 +154,10 @@ def get_args(**kwargs):
     parser.add_argument('-classes', type=int, default=80, help='dataset classes')
     parser.add_argument('-train_label_path', dest='train_label', type=str, default='train.txt', help="train label path")
     parser.add_argument(
-        '-optimizer', type=str, default='adam',
-        help='training optimizer',
-        dest='TRAIN_OPTIMIZER')
-    parser.add_argument(
         '-iou-type', type=str, default='iou',
         help='iou type (iou, giou, diou, ciou)',
         dest='iou_type')
-    parser.add_argument(
-        '-keep-checkpoint-max', type=int, default=10,
-        help='maximum number of checkpoints to keep. If set 0, all checkpoints will be kept',
-        dest='keep_checkpoint_max')
+    parser.add_argument('-batch_size',type=int, default=4)
     args = vars(parser.parse_args())
 
     # for k in args.keys():
